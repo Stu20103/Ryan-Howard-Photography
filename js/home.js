4d317efa4ad32c7c503c28243b0e387dd199c8c0
stu20103 <stu20103@ashs.school.nz>
2022-08-16 12:27:17 +1200

    testing

--- js/home.js
+++ js/home.js
@@ -1,9 +1,9 @@
 $.backstretch([
-      "Images/lambo.JPG"
+    "images/coolcarsmoke.jpg"
+    , "Images/lambo.JPG"
     , "Images/lambointerior.JPG"
     , "Images/wgt3.jpg"
     , "Images/sideswipe.jpg"
     , "images/redcarsmoke.jpg"
-    , "images/coolcarsmoke.jpg"
     , "Images/lambodoor.jpg"
 ], {duration: 3000, fade: 750});
